238c03509a6e6cb51c57cefe9edb93309657b123
Ariel Keselman <skariel@gmail.com>
2014-11-03 14:28:17 +0200

    fixing tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,6 +11,6 @@ insert!(q, Point(0.9, 0.9))
 @test !q.lxly.is_empty
 @test !q.hxhy.is_divided
 @test !q.hxhy.is_empty
-@test q.lxly.is_divided
-@test q.lxly.is_empty
+@test q.is_divided
+@test q.is_empty
 
