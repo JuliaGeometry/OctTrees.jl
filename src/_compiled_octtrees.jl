eb24ba42df82f396c5b7f6c055e238cde96187be
Ariel Keselman <skariel@gmail.com>
2015-02-22 23:51:13 +0200

    better iteration through compiled trees

--- src/_compiled_octtrees.jl
+++ src/_compiled_octtrees.jl
@@ -10,7 +10,7 @@ CompiledOctTreeNode{T<:AbstractPoint3D}(point::T, l::Float64, next::Int64) =
 CompiledOctTreeNode{T<:AbstractPoint3D}(n::OctTreeNode{T}) =
         CompiledOctTreeNode{T}(n.point, isleaf(n) ? -1.0 : 2.0*n.r, -1)
 
-withnext{T<:AbstractPoint3D}(cn::CompiledOctTreeNode{T}, next::Int64) =
+@inline withnext{T<:AbstractPoint3D}(cn::CompiledOctTreeNode{T}, next::Int64) =
     CompiledOctTreeNode{T}(cn.point, cn.l, next)
 
 type CompiledOctTree{T<:AbstractPoint3D} <: SpatialTree
@@ -32,12 +32,20 @@ end
 function compile!{T<:AbstractPoint3D}(ct::CompiledOctTree{T}, t::OctTree{T})
     ct.number_of_nodes_used = 0
     map(t, ct)
+    childs = Array(OctTreeNode{T}, 8)
 
     # fix neighbours
     @inbounds for i in 1:t.number_of_nodes_used
         const q=t.nodes[i]
         !q.is_divided && continue
-        childs = (q.lxlylz, q.lxlyhz, q.lxhylz, q.lxhyhz, q.hxlylz, q.hxlyhz, q.hxhylz, q.hxhyhz)
+        childs[1] = q.lxlylz
+        childs[2] = q.lxlyhz
+        childs[3] = q.lxhylz
+        childs[4] = q.lxhyhz
+        childs[5] = q.hxlylz
+        childs[6] = q.hxlyhz
+        childs[7] = q.hxhylz
+        childs[8] = q.hxhyhz
         for a in 1:7
             const qa = childs[a]
             qa.id <= 0 && continue
